aa7402e27bac098304f19cd7a5ebfc9697d6c0ed
Chanuka Dilhara Mullevidana <62568997+chanukaHub@users.noreply.github.com>
2023-12-17 00:02:40 +0530

    finished linking youtube video

--- app/src/main/java/com/chanuwa/foody/activities/MealActivity.kt
+++ app/src/main/java/com/chanuwa/foody/activities/MealActivity.kt
@@ -1,7 +1,10 @@
 package com.chanuwa.foody.activities
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
@@ -16,6 +19,7 @@ class MealActivity : AppCompatActivity() {
     private lateinit var mealName:String
     private lateinit var mealThumb:String
     private lateinit var binding: ActivityMealBinding
+    private lateinit var youtubeLink:String
     private lateinit var mealMvvm: MealViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,16 +30,29 @@ class MealActivity : AppCompatActivity() {
         mealMvvm = ViewModelProvider(this)[MealViewModel::class.java]
         getMealInformationFromIntent()
         setInformationInView()
+
+        loadingCase()
         mealMvvm.getMealDetail(mealId)
         observerMealDetailsLiveData()
+
+        onYoutubeImageClick()
+    }
+
+    private fun onYoutubeImageClick() {
+        binding.imgYoutube.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
     }
 
     private fun observerMealDetailsLiveData() {
         mealMvvm.observerMealDetailsLiveData().observe(this,object : Observer<MealListModel.Meal>{
             override fun onChanged(meal: MealListModel.Meal) {
+                onResponseCase()
                 binding.tvCategory.text = "Category : ${meal!!.strCategory}"
                 binding.tvArea.text = "Area : ${meal!!.strArea}"
                 binding.tvInstructionSteps.text = meal.strInstructions
+                youtubeLink = meal.strYoutube!!
             }
 
         })
@@ -55,5 +72,23 @@ class MealActivity : AppCompatActivity() {
         mealId = intent.getStringExtra(HomeFragment.MEAL_ID)!!
         mealName = intent.getStringExtra(HomeFragment.MEAL_NAME)!!
         mealThumb = intent.getStringExtra(HomeFragment.MEAL_THUMB)!!
+    }
+
+    private fun loadingCase(){
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnAddToFav.visibility = View.INVISIBLE
+        binding.tvInstruction.visibility = View.INVISIBLE
+        binding.tvCategory.visibility = View.INVISIBLE
+        binding.tvArea.visibility = View.INVISIBLE
+        binding.imgYoutube.visibility = View.INVISIBLE
+    }
+
+    private fun onResponseCase(){
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.btnAddToFav.visibility = View.VISIBLE
+        binding.tvInstruction.visibility = View.VISIBLE
+        binding.tvCategory.visibility = View.VISIBLE
+        binding.tvArea.visibility = View.VISIBLE
+        binding.imgYoutube.visibility = View.VISIBLE
     }
 }
